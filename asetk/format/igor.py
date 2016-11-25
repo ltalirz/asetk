3889e9c3494ced9d75fdc0b5ee997fed1a53f531
Leopold Talirz <leopold.talirz@gmail.com>
2016-11-25 00:49:20 +0000

    added feature to read igor files
    tested only for 2d

--- asetk/format/igor.py
+++ asetk/format/igor.py
@@ -2,30 +2,40 @@
 
 """
 
-#import re
-#import copy  as cp
+import re
 import numpy as np
-#import StringIO
-#import asetk.atomistic.fundamental as fu
-#import asetk.atomistic.constants as atc
 from . import cube
 
 class Axis(object):
     """Represents an axis of an IGOR wave"""
 
-    def __init__(self, symbol, min, max, unit, wavename=None):
+    def __init__(self, symbol, min, delta, unit, wavename=None):
         self.symbol = symbol
         self.min = min
-        self.max = max
+        self.delta = delta
         self.unit = unit
         self.wavename = wavename
 
     def __str__(self):
-        max = 0 if self.max is None else self.max
-        s = "X SetScale {symb} {min},{max}, \"{unit}\", {name};\n"\
-              .format(symb=self.symbol, min=self.min, max=max,\
+        delta = 0 if self.delta is None else self.delta
+        s = "X SetScale {symb} {min},{delta}, \"{unit}\", {name};\n"\
+              .format(symb=self.symbol, min=self.min, delta=delta,\
                       unit=self.unit, name=self.wavename)
         return s
+
+    def read(self, string):
+        """Read axis from string
+
+        Format: 
+        X SetScale/P x 0,2.01342281879195e-11,"m", data_00381_Up;
+        SetScale d 0,0,"V", data_00381_Up
+        """
+        match = re.search("SetScale/?P? (.) ([+-\.\de]+),([+-\.\de]+),\"(\w+)\",\s*(\w+)", string)
+        self.symbol = match.group(1)
+        self.min = float(match.group(2))
+        self.delta = float(match.group(3))
+        self.unit = match.group(4)
+        self.wavename = match.group(5)
 
 
 
@@ -56,6 +66,70 @@ class Wave(object):
             s += str(ax)
         return s
 
+    def read(self, fname):
+        """Read IGOR wave
+        
+        Should work for any dimension.
+        Tested so far only for 2d wave.
+        """
+        f=open(fname, 'r')
+        content=f.read()
+        f.close()
+
+        lines = content.split("\r")
+
+        line = lines.pop(0)
+        if not line == "IGOR":
+            raise IOError("Files does not begin with 'IGOR'")
+
+        line = lines.pop(0)
+        while not re.match("WAVES",line):
+            line = lines.pop(0)
+        match = re.search("WAVES/N=\(([\d,]+)\)\s+(.+)",line)
+        grid = match.group(1).split(',')
+        grid = np.array(grid, dtype=int)
+        self.name = match.group(2)
+
+        line = lines.pop(0)
+        if not line == "BEGIN":
+            raise IOError("Missing 'BEGIN' statement of data block")
+
+        # read data
+        datastring = ""
+        line = lines.pop(0)
+        while not re.match("END",line):
+            datastring += line
+            line = lines.pop(0)
+        data = np.array(datastring.split(), dtype=float)
+        self.data = data.reshape(grid)
+
+        # read axes
+        line = lines.pop(0)
+        print(line)
+        matches = re.findall("SetScale.+?(?:;|$)", line)
+        self.axes = []
+        for match in matches:
+            ax = Axis(None,None,None,None)
+            ax.read(match)
+            self.axes.append(ax)
+
+        # the rest is discarded...
+        #line = lines.pop(0)
+        #print(line)
+
+    @property
+    def extent(self):
+        """Returns extent for plotting"""
+        grid = self.data.shape
+        extent = []
+        for i in range(len(grid)):
+            ax = self.axes[i]
+            extent.append(ax.min)
+            extent.append(ax.min+ax.delta*grid[i])
+
+        return np.array(extent)
+
+
     def print_data(self):
         """Determines how to print the data block.
         
@@ -72,7 +146,7 @@ class Wave1d(Wave):
     
     default_parameters = dict(
         xmin = 0.0,
-        xmax = None,
+        xdelta = None,
         xlabel = 'x',
         ylabel = 'y',
     )
@@ -90,7 +164,7 @@ class Wave1d(Wave):
 
         if axes is None:
             p=self.parameters
-            x = Axis(symbol='x', min=p['xmin'], max=p['xmax'], unit=p['xlabel'],
+            x = Axis(symbol='x', min=p['xmin'], delta=p['xdelta'], unit=p['xlabel'],
                     wavename=self.name)
             self.axes = [x]
 
@@ -108,10 +182,10 @@ class Wave2d(Wave):
 
     default_parameters = dict(
         xmin = 0.0,
-        xmax = 1.0,
+        xdelta = 1.0,
         xlabel = 'x',
         ymin = 0.0,
-        ymax = 1.0,
+        ydelta = 1.0,
         ylabel = 'y',
     )
  
@@ -123,8 +197,8 @@ class Wave2d(Wave):
         
          * data 
          * name 
-         * xmin, xmax, xlabel         
-         * ymin, ymax, ylabel         
+         * xmin, xdelta, xlabel         
+         * ymin, ydelta, ylabel         
         """
         super(Wave2d, self).__init__(data, axes=axes, name=name)
 
@@ -137,9 +211,9 @@ class Wave2d(Wave):
 
         if axes is None:
             p=self.parameters
-            x = Axis(symbol='x', min=p['xmin'], max=p['xmax'], 
+            x = Axis(symbol='x', min=p['xmin'], delta=p['xdelta'], 
                      unit=p['xlabel'], wavename=self.name)
-            y = Axis(symbol='y', min=p['ymin'], max=p['ymax'], 
+            y = Axis(symbol='y', min=p['ymin'], delta=p['ydelta'], 
                      unit=p['ylabel'], wavename=self.name)
             self.axes = [x,y]
 
@@ -208,28 +282,28 @@ class Wave3d(Wave):
         axes.append(Axis(
             symbol='x',
             min=c.origin[0],
-            max=c.origin[0] + c.cell[0][0],
+            delta=c.dx,
             label="x [Bohr]",
             name=self.name)
             )
         axes.append(Axis(
             symbol='y',
             min=c.origin[1],
-            max=c.origin[1] + c.cell[1][1],
+            delta=c.dy,
             label="y [Bohr]",
             name=self.name)
             )
         axes.append(Axis(
             symbol='z',
             min=c.origin[2],
-            max=c.origin[2] + c.cell[2][2],
+            delta=c.dz,
             label="z [Bohr]",
             name=self.name)
             )
         axes.append(Axis(
             symbol='d',
             min=np.min(d.data),
-            max=np.max(d.data),
+            delta=0,
             label="data [Unknown]",
             name=self.name)
             )
@@ -275,5 +349,3 @@ class Wave3d(Wave):
 #            match = re.search(commentregex, self.comment)
 #            self.wfn = int(match.group(1))
 #            self.spin = int(match.group(2))
-
-
